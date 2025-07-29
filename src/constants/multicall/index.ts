7a701fb59a89b64b0f88949af2011108069f0013
BitBD <bitbd83@gmail.com>
2025-07-29 19:04:40 +0600

    update to multicall3

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@cheeseswap/cheeseswap-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
+  [ChainId.MAINNET]: '0x66E5c8Bd353ecB02e0141aB2e3829AD232009530', // multicall3 updated
   [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
 }
 
--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -2,6 +2,6 @@ import { Web3Provider } from '@ethersproject/providers'
 
 export default function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider)
-  library.pollingInterval = 15000
+  library.pollingInterval = 5000
   return library
 }
