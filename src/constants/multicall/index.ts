c38c54b879f7dc8ba37c04540bbd932e2c3837c6
BitBD <bitbd83@gmail.com>
2025-07-29 22:27:14 +0600

    multicall1en

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@cheeseswap/cheeseswap-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x52E54D127cA6474aB1C700335C4714f2515b4f3D', // multicall1En updated
+  [ChainId.MAINNET]: '0x50B321a3519332ccbDb30d4c864C5DD3d4E85962', // multicall1En updated
   [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
 }
 
